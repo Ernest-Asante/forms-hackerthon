730b44fd4825d2faef8c9182ff2442f4b64fb9a9
Ernest-Asante <106278487+Ernest-Asante@users.noreply.github.com>
2025-03-15 08:45:44 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { FormProvider } from "./DOCS/formContext"; // Import the context provider
+import { FormProvider } from "./DOCS/formContext"; 
 import "./App.css";
 import LoginPage from "./DOCS/Login";
 import Dashboard from "./DOCS/Dashboard";
 import Createform from "./DOCS/Createform";
-import FormPage from "./DOCS/FormPage"; // Import the new FormPage component
+import FormPage from "./DOCS/FormPage"; 
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
           <Route path="/" element={<LoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/create-form" element={<Createform />} />
-          <Route path="/forms/:id" element={<FormPage />} /> {/* Add this route */}
+          <Route path="/forms/:id" element={<FormPage />} /> 
         </Routes>
       </FormProvider>
     </Router>
