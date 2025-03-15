9e155c6e8c54bee232177e28a22e407003d2c030
Ernest-Asante <106278487+Ernest-Asante@users.noreply.github.com>
2025-03-15 09:16:43 +0000

    Update formContext.js

--- src/DOCS/formContext.js
+++ src/DOCS/formContext.js
@@ -1,9 +1,9 @@
 import React, { createContext, useState } from "react";
 
-// Create a context for form data
+
 export const FormContext = createContext();
 
-// Create a provider component
+
 export const FormProvider = ({ children }) => {
   const [formData, setFormData] = useState({
     title: "",
