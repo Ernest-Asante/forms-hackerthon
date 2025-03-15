5eb123d77b5bb5c1479ac2c5a7c0faf26420fa7e
Ernest-Asante <106278487+Ernest-Asante@users.noreply.github.com>
2025-03-15 09:26:28 +0000

    Update firebase-config.jsx

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -1,11 +1,11 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; // Import GoogleAuthProvider
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyANk4tOk7yr0WYu9_513D3Nf3LW3qJQUYo",
   authDomain: "mylearning-hub.firebaseapp.com",
@@ -16,10 +16,10 @@ const firebaseConfig = {
   measurementId: "G-H2EVSSSPQ8",
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider(); // Initialize GoogleAuthProvider
+export const googleProvider = new GoogleAuthProvider(); 
